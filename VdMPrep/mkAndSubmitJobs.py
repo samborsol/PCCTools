7cd4a799c590abefa325a63f55c7b1ef561cef16
Jakob <jakobsalfeld@cern.ch>
2016-01-27 01:03:03 +0100

    fix path to input files

--- VdMPrep/mkAndSubmitJobs.py
+++ VdMPrep/mkAndSubmitJobs.py
@@ -7,6 +7,7 @@ import subprocess
 parser=argparse.ArgumentParser()
 parser.add_argument("-p",  "--path",  help="EOS path to PCCNTuples... /store/user/..")
 parser.add_argument("-d",  "--dir", default="JobsDir", help="Output directory")
+#parser.add_argument("-de",  "--direos", default="", help="Output Eos directory")
 parser.add_argument('--label', type=str, default="", help="Label for output file")
 parser.add_argument('--mintime', type=float, default=0, help="Minimum time stamp")
 parser.add_argument('--maxtime', type=float, default=math.pow(2,66), help="Maximum time stamp")
@@ -21,8 +22,9 @@ def MakeJob(outputdir,jobid,filename,mintime,maxtime):
     joblines.append("source /cvmfs/cms.cern.ch/cmsset_default.sh")
     joblines.append("cd "+outputdir)
     joblines.append("cmsenv")
-    joblines.append("python ../makeVdMMiniTree.py --pccfile="+filename+" --label="+args.label+"_"+str(jobid)+" --mintime="+str(mintime)+" --maxtime="+str(maxtime)+" --vetoModules=../"+args.vetoModules)
-    
+    #joblines.append("eval `scramv1 runtime -sh`")
+    joblines.append("python ../makeVdMMiniTree.py --pccfile="+args.path+filename+" --label="+args.label+"_"+str(jobid)+" --mintime="+str(mintime)+" --maxtime="+str(maxtime)+" --vetoModules=../"+args.vetoModules)
+    #joblines.append("cmsStage *.root "+args.direos)
     scriptFile=open(outputdir+"/job_"+str(jobid)+".sh","w+")
     for line in joblines:
         scriptFile.write(line+"\n")
@@ -41,18 +43,21 @@ def SubmitJob(job,queue="8nh"):
 # ls the eos directory
 fileinfos=subprocess.check_output(["cmsLs", args.path])
 fileinfos=fileinfos.split("\n")
+print fileinfos
 
 filenames={}
 for fileinfo in fileinfos:
     info=fileinfo.split()
-    if len(info)<4:
-        continue
-    filename=info[4]
+    #if len(info)<4:
+    #    continue
+    filename=fileinfo
     if filename.find(".root") == -1:
         continue
     jobid=filename.split("/")[-1].split(".")[0].split("_")[-1]
     #print jobid, filename
     filenames[int(jobid)]=filename
+
+print filenames
 
 fullOutPath=os.getcwd()
 if not os.path.exists(args.dir):
