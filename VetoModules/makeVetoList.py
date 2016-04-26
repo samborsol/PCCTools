b72664b0071a23d7fbd9d26abbe46f2f302b633f
Chris Palmer <capalmer@srv-C2C03-20.cms>
2016-04-26 05:14:15 +0200

    update with instructions

--- VetoModules/makeVetoList.py
+++ VetoModules/makeVetoList.py
@@ -11,6 +11,10 @@ if args.runs=="":
     print "No runs to search.  Exiting."
     sys.exit(1)
 
+##1) Identify a run number associated with the fill. 
+##For example for fill 4634 run 262081. 
+##You can take them from WBM for the fill you are interested in.
+##https://cmswbm.web.cern.ch/cmswbm/cmsdb/servlet/FillReport
 runs=args.runs.split(",")
 
 globalConfigKeys=[]
@@ -33,6 +37,11 @@ for modLine in modLines:
         print "Can't do",modLine
 
 
+##2) Login to srv-c2f38-16-01. (Requires cmsusr account/login first.) 
+##Look into the file 
+##/pixel/data0/Run_262000/Run_262081/PixelConfigurationKey.txt 
+##and note the configuration key number, in this case: 
+##100965
 for run in runs:
     runDir=int(run)/1000
     runDir=runDir*1000
@@ -54,6 +63,19 @@ for run in runs:
     pixelConfigFile.close()
     
 print "globalConfigKeys",globalConfigKeys
+
+
+##3) Search the file 
+##/pixelscratch/pixelscratch/config/Pix/configurations.txt 
+##for the string “key 100965” and note the detconfig version which is just below. 
+## 
+##In this case: 
+##
+##key 100965 
+##detconfig   95 
+##
+##This identifies the version of the detconfig file = 95.  
+
 
 pixelCfgMapFileName="/pixelscratch/pixelscratch/config/Pix/configurations.txt"
 if  os.path.isfile(pixelCfgMapFileName):
@@ -84,6 +106,10 @@ print "detector configurations",detConfigs
 
 badModules=[]
 
+##4) You can see the disabled modules in the file 
+##/pixelscratch/pixelscratch/config/Pix/detconfig/95/detectconfig.dat 
+## 
+##Now you can search for the module with the “noAnalogSignal” string. 
 for detConfig in detConfigs:
     detectorConfigFileName="/pixelscratch/pixelscratch/config/Pix/detconfig/"+detConfig+"/detectconfig.dat"
     detectorConfigFile=open(detectorConfigFileName)
@@ -93,6 +119,7 @@ for detConfig in detConfigs:
 
     for line in lines:
         if line.find("noAnalogSignal")!=-1:
+            ## if any ROC is missing remove the entire module
             module=line.split("_ROC")[0]
             if module not in badModules:
                 badModules.append(module)
@@ -100,6 +127,7 @@ for detConfig in detConfigs:
 badModules.sort()
 print "bad modules",badModules
 
+##write out the module numbers to a file
 outputFile=open(args.output,"w")
 print "Writing bad module numbers to",args.output
 for badModule in badModules:
